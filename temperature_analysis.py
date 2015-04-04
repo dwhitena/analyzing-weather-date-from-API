6f25197086aa133f7ef731f7183f24fa879d16df
dwhitena <whitenack.daniel@gmail.com>
2015-04-04 08:03:10 -0500

    modifying code style

--- temperature_analysis.py
+++ temperature_analysis.py
@@ -8,11 +8,13 @@ import folium
 
 
 # define the locations of cities for this analysis
-cities = {"Chicago": ["41.837551","-87.681844"], 
+cities = {
+     "Chicago": ["41.837551","-87.681844"], 
      "New_York_City": ["40.663619","-73.938589"],
      "Denver": ["39.761850","-104.881105"],
      "Los_Angeles": ["34.019394","-118.410825"],
-     "Austin": ["30.303936","-97.754355"]}
+     "Austin": ["30.303936","-97.754355"]
+     }
 
 # connect to database
 con = lite.connect('weather.db')
@@ -85,8 +87,8 @@ map_2 = folium.Map(location=[40, -98], width=750, height=500, zoom_start=4)
 for k in cities.keys():
 	area = total_change[k]*1000
 	map_2.circle_marker(location=cities[k], radius=area,
-                    popup=k + ": " + str(total_change[k]) + " degrees", line_color='#3186cc',
-                    fill_color='#3186cc')
+                        popup=k + ": " + str(total_change[k]) + " degrees", line_color='#3186cc',
+                        fill_color='#3186cc')
 map_2.create_map(path='agg_temp_changes_map.html')
 
 # visualize day-to-day swings on us map
@@ -94,6 +96,6 @@ map_2 = folium.Map(location=[40, -98], width=500, height=500, zoom_start=4)
 for k in cities.keys():
 	area = swing_change[k]*7000
 	map_2.circle_marker(location=cities[k], radius=area,
-                    popup=k + ": " + str(swing_change[k]) + " degrees", line_color='#3186cc',
-                    fill_color='#3186cc')
+                        popup=k + ": " + str(swing_change[k]) + " degrees", line_color='#3186cc',
+                        fill_color='#3186cc')
 map_2.create_map(path='swing_temp_changes_map.html')
